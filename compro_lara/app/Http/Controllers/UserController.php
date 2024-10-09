9b05073336b206ae0d141dbae9a596d17e6b73b0
amansj1 <amansejahtera1@gmail.com>
2024-10-09 16:17:33 +0700

    [done]  register feature and login

--- compro_lara/app/Http/Controllers/UserController.php
+++ compro_lara/app/Http/Controllers/UserController.php
@@ -4,36 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use App\User;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
 {
     //
     public function formlogin(){
-        if($request->session()->has('key')){
-            return redirect()->route(); //return to dashboard if already login 
+        return view('loginpage'); // to form login
+     
+    }
+    public function formregis(){
+        return view('register');
+    }
+
+    public function register(Request $req){
+        // dd($req);
+
+
+        $createuser = User::create([
+            'name' => $req->name,   
+            'email' => $req->email,
+            'password' => Hash::make($req->password)
+        ]);
+        if($createuser){
+            return redirect()->route('formlogin')->with('status', "Register Success");
         }else{
-            return view(); // to form login
+            return redirect()->route('formregister')->with('status', "Register Failed");
         }
     }
 
     public function login(Request $req){
-        if($request->session()->has('key')){
+        if($req->session()->has('name')){
             return redirect()->route(); //return to logout if already login 
         }
         $getuser = User::where('email',$req->email)->first();
+    dd($getuser);
         if($getuser != null && Hash::check($req->password, $getuser->password)){
             //return to dashboard
+           dd('0');
         }else{
             //return to formlogin
             $pass = Hash::make($req->password);
-                if($req->nip == $getuser->nip && Hash::check($req->password, $getuser->password)){
+                if($req->email == $getuser->email && Hash::check($req->password, $getuser->password)){
                     Session::put('user_id', $req->id);
                     Session::put('name', $getuser->name);
-                    Session::put('role', $getuser->role);
                     // dd(Session::get('role'));
-                    return redirect()->route('')->with('status', "Login Success");
+                    dd($getuser);
+                    return redirect()->route('home')->with('status', "Login Success");
         }
     }
     }
